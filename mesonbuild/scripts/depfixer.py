d040d2ee54134481b18413d950f2958eab6d7734
Nirbheek Chauhan <nirbheek@centricular.com>
2020-02-12 02:29:09 +0530

    depfixer: Cache searching of install_name_tool
    This gives a significant speedup in large projects such as gst-build
    since now we only search for the tool once. Speed-up on Windows:
    
    ```
    meson install:
    before: 15.3 seconds
    after:   5.4 seconds
    
    meson install --only-changed:
    before: 11.6 seconds
    after:   2.7 seconds
    ```

--- mesonbuild/scripts/depfixer.py
+++ mesonbuild/scripts/depfixer.py
@@ -26,6 +26,9 @@ DT_STRTAB = 5
 DT_SONAME = 14
 DT_MIPS_RLD_MAP_REL = 1879048245
 
+# Global cache for tools
+INSTALL_NAME_TOOL = False
+
 class DataSizes:
     def __init__(self, ptrsize, is_le):
         if is_le:
@@ -428,6 +431,7 @@ def fix_jar(fname):
     subprocess.check_call(['jar', 'ufm', fname, 'META-INF/MANIFEST.MF'])
 
 def fix_rpath(fname, new_rpath, final_path, install_name_mappings, verbose=True):
+    global INSTALL_NAME_TOOL
     # Static libraries never have rpaths
     if fname.endswith('.a'):
         return
@@ -445,5 +449,7 @@ def fix_rpath(fname, new_rpath, final_path, install_name_mappings, verbose=True)
             pass
         else:
             raise
-    if shutil.which('install_name_tool'):
+    if INSTALL_NAME_TOOL is False:
+        INSTALL_NAME_TOOL = shutil.which('install_name_tool')
+    if INSTALL_NAME_TOOL:
         fix_darwin(fname, new_rpath, final_path, install_name_mappings)
